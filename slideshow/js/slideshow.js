07bccd3966409eaa05cabbb046a1c41055b2d97e
trilobit.alex <trilobit.alex@d29d7d02-2744-11df-a38d-a3bc082ccce7>
2010-10-05 01:25:33 +0000

    Slideshow

--- slideshow/js/slideshow.js
+++ slideshow/js/slideshow.js
@@ -1,56 +1,22 @@
+var slides       = [];
+var preload      = [];
 var alphaPath    = '/common/slideshow/fade/';  // path to fade images
-
-var slideSpeed   = 0;                          // delay time
-var slideWidth   = 0;                          // slide width
-var slideHeight  = 0;                          // slide height
 var alphaImgs    = [];                         // fade images
+var slideSpeed   = 0;                          // delay time
 var iCurrStep    = 0;                          // step of fading
+var iCurrSlide   = 0;                          // step of fading
 var slideCount   = 0;                          // count of slides
 var showSlides   = false;
 
+
 function slideShow(delay,widthSlide,heightSlide){
+  if(delay < 1000) delay = 1000;
   $(document).ready(function() {
-    slideSpeed  = delay;
-    slideWidth  = widthSlide;
-    slideHeight = heightSlide;
-    startShow();
+    prepareSlideShow(delay,widthSlide,heightSlide);
   });
 }
 
-// check images load
-function checkImg(fadeNext){
-  var imgCounter=0;
-  $('ul#slideshow li img').each(function(){
-    if(this.complete==true && this.width>0) imgCounter++;
-  });
-  if(imgCounter>=slideCount){
-    $('#slideshowFade img').attr('src',alphaImgs[0].src);
-    showSlides = true;
-    if(fadeNext==true)
-      setTimeout('fade()',slideSpeed);
-    else
-      setTimeout('gallery()',slideSpeed);
-  }else
-    timer = setTimeout('checkImg('+fadeNext+')',500);
-}
-// fade image
-function fade(){
-  if(showSlides==false){
-    setTimeout('fade()',100);
-    return;
-  }
-  iCurrStep++;
-  if(iCurrStep>4){
-    iCurrStep = 0;
-    $('#slideshowFade img').attr('src',alphaImgs[iCurrStep].src);
-    gallery();
-  }else{
-    $('#slideshowFade img').attr('src',alphaImgs[iCurrStep].src);
-    setTimeout('fade()',100);
-  }
-}
-
-function startShow() {
+function prepareSlideShow(delay,widthSlide,heightSlide) {
   // load fade images
   alphaImgs[0] = new Image();
   alphaImgs[1] = new Image();
@@ -66,37 +32,51 @@ function startShow() {
   alphaImgs[5].src = alphaPath + 'fade_100.png';
   
   // get count of slides
-  var slideCount = $('ul#slideshow').children().size();
+  slideCount = $('ul#slideshow').children().size();
   
-  //show first LI
-  var current = $('ul#slideshow li:first');
-  current.addClass('show');
+  $('ul#slideshow').css("width",widthSlide);
+  $('ul#slideshow').css("height",heightSlide);
+  
+  $('ul#slideshow li').each(function(){
+    _src    = $(this).find('img').attr('src');
+    _descr  = $(this).find('img').attr('alt');
+    _title  = $(this).find('img').attr('title');
+    _button = $(this).find('a').attr('title');
+    _href   = $(this).find('a').attr('href');
+    slides.push({'src': _src, 'href': _href, 'title': _title, 'descr': _descr, 'button': _button});
+  });
+  $('ul#slideshow li').remove();
+  
+  // load fade images
+  n = (slideCount>5 ? 5 : slideCount);
+  for(i=0; i<n; i++){
+    preload[i] = new Image();
+    preload[i].src = slides[i].src;
+  }
+
+  slideSpeed = delay;
   
   //append a LI item to the UL list for displaying caption
-  $('ul#slideshow').append('<li id="slideshowCaption"><div><h3></h3><p></p></div></li>');
-  $('ul#slideshow').append('<li id="slideshowButton"><a></a><p></p></li>');
-  $('ul#slideshow').append('<li id="slideshowFade"><a><img src="'+alphaImgs[5].src+'" width="'+slideWidth+'" height="'+slideHeight+'" border="0" /></a></li>');
+  $('ul#slideshow').append('<li id="slideshowCaption" style="width:'+widthSlide+'px;"><div><h3></h3><p></p></div></li>');
+  $('ul#slideshow').append('<li id="slideshowButton"><a></a></li>');
+  $('ul#slideshow').append('<li id="slideshowFade"><a><img src="'+alphaImgs[0].src+'" width="'+widthSlide+'" height="'+heightSlide+'" border="0" /></a></li>');
+  $('ul#slideshow').append('<li id="slideshowSlide"><img src="'+slides[0].src+'" width="'+widthSlide+'" height="'+heightSlide+'" border="0" /></li>');
   $('ul#slideshow').show();
 
   //Get the caption of the first image from REL attribute and display it
-  $('#slideshowCaption h3').html($('ul#slideshow a:first').find('img').attr('title'));
-  $('#slideshowCaption p').html($('ul#slideshow a:first').find('img').attr('alt'));
-  $('#slideshowButton a').html($('ul#slideshow a:first').attr('title'));
-  $('#slideshowButton a').attr('href',$('ul#slideshow a:first').attr('href'));
-  $('#slideshowFade a').attr('href',$('ul#slideshow a:first').attr('href'));
+  $('#slideshowCaption h3').html(slides[0].title);
+  $('#slideshowCaption p').html(slides[0].descr);
+  $('#slideshowButton a').html(slides[0].button);
+  $('#slideshowButton a').attr('href',slides[0].href);
+  $('#slideshowFade a').attr('href',slides[0].href);
 
   //Display the caption
-  $('#slideshowCaption').css({opacity: 0.7, bottom:0});
+  //$('#slideshowCaption').css({opacity: 0.7, bottom:0});
   //Display the button
-  $('#slideshowButton').css({opacity: 1.0, bottom:0});
+  //$('#slideshowButton').css({opacity: 1.0, bottom:0});
 
-  var href = current.find('a').attr('href');
-  if(current.next().attr('id') == 'slideshowCaption'){
-    var next = $('ul#slideshow li:first');
-  }else{
-    var next = current.next();
-  }
-  var nextHref = next.find('a').attr('href');
+  var href = slides[0].href;
+  var nextHref = slides[1].href;
 
   //pause the slideshow on mouse over
   $('ul#slideshow').hover(
@@ -110,41 +90,80 @@ function startShow() {
   checkImg(href!=nextHref ? true : false);
 }
 
+// check images load
+function checkImg(fadeNext){
+  var imgCounter=0;
+  for(i=0; i<6; i++){
+    if(alphaImgs[i].complete==true && alphaImgs[i].width>0) imgCounter++;
+  }
+  for(i=0; i<preload.length; i++){
+    if(preload[i].complete==true && preload[i].width>0) imgCounter++;
+  }
+  n = (slideCount>5 ? 5 : slideCount);
+  if(imgCounter-6>=n){
+    $('#slideshowSlide').addClass('show');
+    showSlides = true;
+    if(fadeNext==true)
+      setTimeout('fade()',slideSpeed);
+    else
+      setTimeout('gallery()',slideSpeed);
+  }else
+    timer = setTimeout('checkImg('+fadeNext+')',500);
+}
+
+// fade image
+function fade(){
+  if(showSlides==false){
+    setTimeout('fade()',100);
+    return;
+  }
+  iCurrStep++;
+  if(iCurrStep>4){
+    iCurrStep = 0;
+    gallery();
+  }else{
+    $('#slideshowFade img').attr('src',alphaImgs[iCurrStep].src);
+    setTimeout('fade()',100);
+  }
+}
+
 function gallery() {
   if(showSlides==false){
     setTimeout('gallery()',slideSpeed);
     return;
   }
-  //if no IMGs have the show class, grab the first image
-  var current = ($('ul#slideshow li.show')?  $('ul#slideshow li.show') : $('ul#slideshow li:first'));
-  if(current.next().attr('id') == 'slideshowCaption'){
-    var next = $('ul#slideshow li:first');
-  }else{
-    var next = current.next();
-  }
-  //Get next image caption
-  var title = next.find('img').attr('title');
-  var desc = next.find('img').attr('alt');
-  var button = next.find('a').attr('title');
-  var href = next.find('a').attr('href');
 
-  if(next.next().attr('id') == 'slideshowCaption'){
-    var nextnext = $('ul#slideshow li:first');
-  }else{
-    var nextnext = next.next();
-  }
-  var nextLink = nextnext.find('a').attr('href');
+  iCurrSlide++;
+  if(iCurrSlide>=slideCount) iCurrSlide = 0;
+  //Get next image caption
+  var title = slides[iCurrSlide].title;
+  var desc = slides[iCurrSlide].descr;
+  var button = slides[iCurrSlide].button;
+  var href = slides[iCurrSlide].href;
+  var src = slides[iCurrSlide].src;
+
+  var next = iCurrSlide + 1;
+  if(next>=slideCount) next = 0;
+  var nextLink = slides[next].href;
 
   $('#slideshowCaption h3').html(title);
   $('#slideshowCaption p').html(desc);
   $('#slideshowButton a').html(button);
   $('#slideshowButton a').attr('href',href);
   $('#slideshowFade a').attr('href',href);
+  $('#slideshowSlide img').attr('src',src);
 
-  current.removeClass('show');
-  next.addClass('show');
+  if(preload.length<slideCount){
+    preload[iCurrSlide+4] = new Image();
+    preload[iCurrSlide+4] = slides[iCurrSlide+4].src;
+  }
+  
+  setTimeout('fade_wait('+(nextLink==href)+')',50);
+}
 
-  if(nextLink!=href)
+function fade_wait(flag){
+  $('#slideshowFade img').attr('src',alphaImgs[0].src);
+  if(flag==false)
     setTimeout('fade()',slideSpeed);
   else
     setTimeout('gallery()',slideSpeed);
